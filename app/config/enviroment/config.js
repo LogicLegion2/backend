99ab544c447916c26ea8e083405555794dcdb2e4
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-07-07 18:18:14 -0500

    Correcciones rutas paypal 2

--- app/config/enviroment/config.js
+++ app/config/enviroment/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = 3000;
-export const HOST = "https://frontend-lg4q.onrender.com";
+export const HOST = "https://backend-barberia-tsn6.onrender.com";
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
 export const PAYPAL_API = "https://api-m.sandbox.paypal.com";
