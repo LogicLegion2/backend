2cdb8af9db7a5ce302cf2d6a5698f34902dcda43
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-07-18 17:21:20 -0500

    Intento docker 1

--- app/config/enviroment/config.js
+++ app/config/enviroment/config.js
@@ -6,6 +6,3 @@ export const HOST = "https://backend-production-64de.up.railway.app";
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
 export const PAYPAL_API = "https://api-m.sandbox.paypal.com";
-// console.log(PAYPAL_API);
-// console.log(PAYPAL_API_CLIENT);
-// console.log(PAYPAL_API_SECRET);
